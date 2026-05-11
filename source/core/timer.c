2b61df2e31b99569fee5d6308f29251a378833e7
fredericotuchtenhag-spec <frederico.tuchtenhag@fueltech.com.br>
2026-05-11 12:44:55 -0300

    Peripheral registers for PWM generation configured, working properly.

--- source/core/timer.c
+++ source/core/timer.c
@@ -1,29 +1,33 @@
 #include "core/timer.h"
+#include "core/system.h"
 #include <libopencm3/stm32/timer.h>
 #include <libopencm3/stm32/rcc.h>
 #include <stdint.h>
 
-#define PRESCALER   (480)
+//freq = APB1_CLK / ((PRESCALER - 1) * (ARR_VALUE - 1))
+
+#define PRESCALER   (240)
 #define ARR_VALUE    (1000)
 
+
 void timer_setup(void) {
-    rcc_periph_clock_enable(RCC_TIM1);
+    rcc_periph_clock_enable(RCC_TIM2);
 
-    timer_set_mode(TIM1, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+    timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
 
-    timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_PWM1);
+    timer_set_oc_mode(TIM2, TIM_OC1, TIM_OCM_PWM1);
 
-    timer_enable_counter(TIM1);
-    timer_enable_oc_output(TIM1, TIM_OC1);
+    timer_enable_counter(TIM2);
+    timer_enable_oc_output(TIM2, TIM_OC1);
 
-    timer_set_prescaler(TIM1, PRESCALER - 1);
-    timer_set_period(TIM1, ARR_VALUE - 1);
+    timer_set_prescaler(TIM2, PRESCALER - 1);
+    timer_set_period(TIM2, ARR_VALUE - 1);
 
 }
 
 void timer_pwm_set_duty_cycle(float duty_cycle) {
     const float raw_value = (float)ARR_VALUE * (duty_cycle / 100.0f);
-    timer_set_oc_value(TIM1, TIM_OC1, (uint32_t)raw_value);
+    timer_set_oc_value(TIM2, TIM_OC1, (uint32_t)raw_value);
 
 }
 
--- source/firmware.c
+++ source/firmware.c
@@ -3,13 +3,14 @@
 #include <libopencm3/stm32/flash.h>
 #include <libopencm3/stm32/rcc.h>
 #include "core/system.h"
+#include "core/timer.h"
 
-#define LEDPORT  (GPIOE)
-#define LEDPIN   (GPIO13)
+#define LEDPORT  (GPIOA)
+#define LEDPIN   (GPIO3)
 
 //testing
 static void gpio_setup(void) { //config for the GPIO Port C pin 13
-	rcc_periph_clock_enable(RCC_GPIOE);
+	rcc_periph_clock_enable(RCC_GPIOA);
 	gpio_mode_setup(LEDPORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LEDPIN);
 	gpio_set_af(LEDPORT, GPIO_AF1, LEDPIN);
 
@@ -19,13 +20,23 @@ int main(void) {
 	system_setup();
 	gpio_setup();
 	timer_setup();
-	float duty_cycle = 0.0f;
-	uint64_t start_time = system_get_ticks();
-	timer_pwm_set_duty_cycle(duty_cycle); 
+	volatile uint32_t apb1_clk_freq = rcc_get_bus_clk_freq(RCC_APB1CLK);
+	volatile uint32_t start_time = system_get_ticks();
 
+	volatile float duty_cycle = 0.0f;
+
+	uint32_t tim2_ccr = 0x40000034; // Address of TIM2_CCR1 register
+	volatile uint32_t *tim2_ccr_ptr = (volatile uint32_t *)tim2_ccr;
+	uint32_t tim2_arr = 0x4000002C; // Address of TIM2_ARR register
+	volatile uint32_t *tim2_arr_ptr = (volatile uint32_t *)tim2_arr;
+
+
+	timer_pwm_set_duty_cycle(duty_cycle); 
+	
 	while (1) {
-		if (system_get_ticks() - start_time >= 1000) { // Update duty cycle every 100 ms
-			duty_cycle += 10.0f; // Increase duty cycle by 10%
+		volatile uint64_t tick_diff = system_get_ticks() - start_time;
+		if (tick_diff >= 500) { // Update duty cycle every 100 ms
+			duty_cycle += 10.0f; // Increase duty cycle by 1%
 			if (duty_cycle > 100.0f) {
 				duty_cycle = 0.0f; // Reset to 1% after reaching 100%
 			}
